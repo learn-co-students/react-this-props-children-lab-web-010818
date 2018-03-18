e7d332506f5cec75557bc54dedf8819cba6df5d0
matjack9 <matjack9@gmail.com>
2018-03-18 14:05:18 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,12 @@
-// Code Invitation Component Here
+import React from 'react';
+
+const Invitation = props => {
+  return (
+    <div>
+      <h1>You've been invited!</h1>
+      <div>{props.children}</div>
+    </div>
+  )
+}
+
+export default Invitation;
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,17 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+
+const ThemedDecorations = props => {
+  const childrenWithTheme = React.Children.map(props.children, child => {
+    return (
+      <div className={props.theme}>{child}</div>
+    )
+  })
+
+  return (
+    <div>
+      {childrenWithTheme}
+    </div>
+  )
+}
+
+export default ThemedDecorations;
